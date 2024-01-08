594b4e909ff1969c60d59e91d5b28713380ef0bb
Ashish0016op <raccoonop0016@gmail.com>
2024-01-08 15:33:28 +0530

    props

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,9 +1,21 @@
-import './App.css';
-import ExpenseItem from './components/ExpenseItem';
+import "./App.css";
+import ExpenseItem from "./components/ExpenseItem";
 function App() {
+  const expenses = [
+    { title: "Food", amount: 10, location: "patna_market " },
+    { title: "Petrol", amount: 100, location: "patna_market" },
+    { title: "Movies", amount: 200, location: "patna_market" },
+  ];
   return (
     <>
-      <ExpenseItem/>
+      <h2>Expenses Itmes</h2>
+      {expenses.map((expense, index) => (
+        <ExpenseItem
+          title={expense.title}
+          amount={expense.amount}
+          location={expense.location}
+        />
+      ))}
     </>
   );
 }
--- my-react-app/src/components/ExpenseItem.js
+++ my-react-app/src/components/ExpenseItem.js
@@ -1,19 +1,13 @@
 import './ExpenseItem.css'
-const About=()=>{
-    const LocationOfExpenditure="patna_market"
+const ExpenseItem=(props)=>{
     return (
         <div className="expense-item">
+            <div>{props.location}</div>
             <div className="expense-item__description">
-                <h2>Food</h2>
-                <div className="expense-item__price">Rs 10</div>
-                <h2>Petrol</h2>
-                <div className="expense-item__price">Rs 100</div>
-                <h2>Movies</h2>
-                <div className="expense-item__price">Rs 200</div>
-                <h2>Location of Items</h2>
-                <div>{LocationOfExpenditure}</div>
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">{props.amount}</div>
             </div>
         </div>
     );
 }
-export default About;
+export default ExpenseItem;
