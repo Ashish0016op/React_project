4c27f1771612b636378e01b3a6b12fa38117cbca
Ashish0016op <raccoonop0016@gmail.com>
2024-01-08 15:05:21 +0530

    location_added

--- my-react-app/src/components/ExpenseItem.js
+++ my-react-app/src/components/ExpenseItem.js
@@ -1,10 +1,18 @@
+import './ExpenseItem.css'
 const About=()=>{
+    const LocationOfExpenditure="patna_market"
     return (
-        <div>
-            <h2>Expense Items</h2>
-            <p>Food Rs 10</p>
-            <p>Petrol Rs 100</p>
-            <p>Movies Rs 200</p>
+        <div className="expense-item">
+            <div className="expense-item__description">
+                <h2>Food</h2>
+                <div className="expense-item__price">Rs 10</div>
+                <h2>Petrol</h2>
+                <div className="expense-item__price">Rs 100</div>
+                <h2>Movies</h2>
+                <div className="expense-item__price">Rs 200</div>
+                <h2>Location of Items</h2>
+                <div>{LocationOfExpenditure}</div>
+            </div>
         </div>
     );
 }
