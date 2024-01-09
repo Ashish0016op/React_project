5d24ce72d56f27a16d787d201f327dde22f4252f
Ashish0016op <raccoonop0016@gmail.com>
2024-01-09 19:46:23 +0530

    update_amount

--- my-react-app/src/components/Expenses/ExpenseItem.js
+++ my-react-app/src/components/Expenses/ExpenseItem.js
@@ -8,21 +8,32 @@ const ExpenseItem=(props)=>{
         fontWeight: 'bold',
       };
       const [isDeleted, setIsDeleted] = useState(false);
-
+      const [title,setTitle]=useState(props.title);
+      const [amount,setAmount]=useState(props.amount);
         const deleteExpenseHandler = () => {
             setIsDeleted(true);
         };
         if (isDeleted) {
             return null;
         }
+        
+        const UpdatTitle=()=>{
+            setTitle('updated');
+            console.log(title);
+        }
+        const UpdateAmount=()=>{
+            setAmount('$100');
+        }
     return (
         <div className="expense-item">
             <ExpenseDate date={props.date}/>
             <div style={locationStyle}>{props.location}</div>
             <div className="expense-item__description">
-                <h2>{props.title}</h2>
-                <div className="expense-item__price">{props.amount}</div>
+                <h2>{title}</h2>
+                <div className="expense-item__price">{amount}</div>
                 <button onClick={deleteExpenseHandler}>Delete Expense</button>
+                <button onClick={UpdatTitle}>Update Title</button>
+                <button onClick={UpdateAmount}>Make $100</button>
             </div>
         </div>
     );
